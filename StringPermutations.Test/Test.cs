4fb739c18008dc114ad57745c6603ab207dae88e
Dmitry Novik <dmitry.novik@gmail.com>
2022-03-07 13:04:23 +1100

    Add project files.

---
+++ StringPermutations.Test/Test.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace StringPermutations.Test
+{
+    public class Test
+    {
+        [Fact] public void ABC()
+        {
+            var all =  Permutations.Compute("abc").OrderBy(s => s);
+
+            all.Should().BeEquivalentTo(new[]
+            {
+                "abc", "acb", "bac", "bca", "cab", "cba",
+            });
+        }
+
+        [Fact] public void ABCD()
+        {
+            var all = Permutations.Compute("abcd").OrderBy(s => s);
+
+            all.Should().BeEquivalentTo(new[]
+            {
+                "abcd",
+                "abdc",
+                "acbd",
+                "acdb",
+                "adbc",
+                "adcb",
+                "bacd",
+                "badc",
+                "bcad",
+                "bcda",
+                "bdac",
+                "bdca",
+                "cabd",
+                "cadb",
+                "cbad",
+                "cbda",
+                "cdab",
+                "cdba",
+                "dabc",
+                "dacb",
+                "dbac",
+                "dbca",
+                "dcab",
+                "dcba",
+            });
+        }
+
+    }
+}
---
+++ StringPermutations/Permutations.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace StringPermutations
+{
+    public static class Permutations
+    {
+        public static IEnumerable<string> Compute(string s) => 
+            string.IsNullOrEmpty(s) ? new[] { s } : Compute(new StringBuilder(s));
+
+        public static IEnumerable<string> Compute(StringBuilder s, int start = 0)
+        {
+            if ((start + 1) >= s.Length)
+                return new[] { s.ToString() };
+
+
+            var results = new List<string>() { s.ToString() };
+            for (var i = start; i < s.Length; ++i)
+            {
+                for (int j = i + 1; j < s.Length; ++j)
+                {
+                    Swap(s, i, j);
+                    var tail = Compute(s, i + 1);
+                    results.AddRange(tail);
+                    Swap(s, i, j);
+                }
+            }
+
+            return results;
+        }
+
+        private static void Swap(StringBuilder s, int i, int j) => (s[j], s[i]) = (s[i], s[j]);
+    }
+}
